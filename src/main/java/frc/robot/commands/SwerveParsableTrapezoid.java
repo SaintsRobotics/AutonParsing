da0a8c5b3956b4f741b8a110e17454b1e2e50370
TiaSinghania <sonicstormwind@gmail.com>
2020-11-16 18:04:14 -0800

    added ProfiledPID command

--- src/main/java/frc/robot/commands/SwerveParsableTrapezoid.java
+++ src/main/java/frc/robot/commands/SwerveParsableTrapezoid.java
@@ -1,10 +1,12 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.SwerveDrivetrain;
@@ -19,7 +21,7 @@ import frc.robot.subsystems.SwerveDrivetrain;
 /**
  * A mock version of the Command
  */
-public class SwerveParsableCommand extends ParsableCommand {
+public class SwerveParsableTrapezoid extends ParsableCommand {
 
     private SwerveDrivetrain m_drivetrain;
 
@@ -28,35 +30,55 @@ public class SwerveParsableCommand extends ParsableCommand {
     private double ySpeed;
     private double rotSpeed;
 
-    private PIDController m_xPID;
-    private PIDController m_yPID;
-    private PIDController m_turningPID;
+    private ProfiledPIDController m_xPID;
+    private ProfiledPIDController m_yPID;
+    private ProfiledPIDController m_rotPID;
 
+    // x PID
+    private TrapezoidProfile.Constraints xConstraints; 
+    
+    // y PID
+    private TrapezoidProfile.Constraints yConstraints;      
+    
+    // turning PID
+    private TrapezoidProfile.Constraints rotConstraints; 
+    
     private Constants m_constants;
 
     private Pose2d m_currPose = new Pose2d(); // default, use odom later
 
-    public SwerveParsableCommand() {
+    public SwerveParsableTrapezoid() {
 
     }
 
-    public SwerveParsableCommand(String params) { // 3,3,30
+    public SwerveParsableTrapezoid(String params) { // 3,3,30
         parse(params);
-        m_constants = new Constants();
+        m_constants = new Constants();        
+        
+        xConstraints = new TrapezoidProfile.Constraints(0, 0);        
+        yConstraints = new TrapezoidProfile.Constraints(0, 0);        
+        rotConstraints = new TrapezoidProfile.Constraints(0, 0);
+        
+        m_xPID = new ProfiledPIDController(Math.toRadians((m_constants.maxMetersPerSecond / 180) * 5), 0, 0, xConstraints);
+        m_yPID = new ProfiledPIDController(Math.toRadians((m_constants.maxMetersPerSecond / 180) * 5), 0, 0, yConstraints);
+        m_rotPID = new ProfiledPIDController(Math.toRadians((m_constants.maxMetersPerSecond / 180) * 5), 0, 0, rotConstraints);
 
-        m_xPID = new PIDController(Math.toRadians((m_constants.maxMetersPerSecond / 180) * 5), 0, 0);
-        m_yPID = new PIDController(Math.toRadians((m_constants.maxMetersPerSecond / 180) * 5), 0, 0);
-        m_turningPID = new PIDController(Math.toRadians((m_constants.maxMetersPerSecond / 180) * 5), 0, 0);
+        m_xPID.reset(0);
+        m_yPID.reset(0);
+        m_rotPID.reset(0);
+
         m_xPID.enableContinuousInput(0, Math.PI * 2);
         m_xPID.setTolerance(1 / 36); 
         m_yPID.enableContinuousInput(0, Math.PI * 2);
         m_yPID.setTolerance(1 / 36); 
-        m_turningPID.enableContinuousInput(0, Math.PI * 2);
-        m_turningPID.setTolerance(1 / 36); 
-        m_turningPID.setSetpoint(targetPosition.getRotation().getRadians());
-        m_xPID.setSetpoint(targetPosition.getTranslation().getX());
-        m_yPID.setSetpoint(targetPosition.getTranslation().getY());
+        m_rotPID.enableContinuousInput(0, Math.PI * 2);
+        m_rotPID.setTolerance(1 / 36); 
 
+        m_rotPID.setGoal(targetPosition.getRotation().getRadians());
+        m_xPID.setGoal(targetPosition.getTranslation().getX());
+        m_yPID.setGoal(targetPosition.getTranslation().getY());
+
+        
         m_drivetrain = new SwerveDrivetrain(m_constants);
     }
 
@@ -75,11 +97,10 @@ public class SwerveParsableCommand extends ParsableCommand {
     }
 
     @Override
-    public void execute() {       
-        rotSpeed = m_turningPID.calculate(m_currPose.getRotation().getRadians());
+    public void execute() {      
+        rotSpeed = m_rotPID.calculate(m_currPose.getRotation().getRadians());
         xSpeed = m_xPID.calculate(m_currPose.getTranslation().getX());
-        ySpeed = m_yPID.calculate(m_currPose.getTranslation().getY());
-
+        ySpeed = m_yPID.calculate(m_currPose.getTranslation().getY());       
 
         m_drivetrain.move(xSpeed, ySpeed, rotSpeed, false);
     }
